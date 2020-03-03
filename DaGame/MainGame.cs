034b269763779b5014901e19fff8d6cd5ad7f6bc
Pinkeyyy <pinkeyyy.uk@gmail.com>
2020-03-03 19:20:48 +0000

    Add Episode 1 codebase

---
+++ DaGame/MainGame.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DaGame
+{
+    class MainGame : Game
+    {
+        private GraphicsDeviceManager manager;
+
+        public MainGame()
+        {
+            manager = new GraphicsDeviceManager(this);
+            IsMouseVisible = true;
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
