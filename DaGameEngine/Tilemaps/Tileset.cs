b91f22daa86a477f059e58ce1e460a39a95c6e01
Pinkeyyy <pinkeyyy.uk@gmail.com>
2020-04-27 20:48:09 +0100

    Add basic JSON for minitileset

--- DaGameEngine/Tilemaps/Tileset.cs
+++ DaGameEngine/Tilemaps/Tileset.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,22 @@ namespace DaGameEngine.Tilemaps
     {
         public string Name { get; set; }
         public Texture2D Texture { get; set; }
+
+        public static Tileset FromJsonFile(string filePath, GraphicsDevice graphicsDevice)
+        {
+            Tileset instance = JsonConvert.DeserializeObject<Tileset>(
+                File.ReadAllText(filePath)
+            );
+
+            string imagePath = Path.ChangeExtension(filePath, ".png");
+
+            using (Stream fileStream = File.OpenRead(imagePath))
+            {
+                instance.Texture = Texture2D.FromStream(graphicsDevice, fileStream);
+            }
+
+            return instance;
+        }
 
         public override string ToString()
         {
